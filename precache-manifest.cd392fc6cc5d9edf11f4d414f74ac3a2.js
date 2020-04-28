975cf8b849432aecb45d434d3a8ba2022b5d2524
pdw63 <paul1.wells@gmail.com>
2020-04-28 16:29:10 +0100

    Updates

---
+++ precache-manifest.cd392fc6cc5d9edf11f4d414f74ac3a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d9e635c2f900d8e16671677bf3af637",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9d83fd339359692a226a",
+    "url": "/monsters-rolodex/static/css/main.c6b19075.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d83fd339359692a226a",
+    "url": "/monsters-rolodex/static/js/main.304cf9b6.chunk.js"
+  },
+  {
+    "revision": "fc1a59712d7f10d18ab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.664c0a0d.js"
+  }
+]);
